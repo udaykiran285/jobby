081557bae6505cdb546ec007a8593abcda4235a1
uday <uday837882gmail.com>
2024-07-02 17:38:59 +0530

    private

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,7 +14,7 @@ const Navbar = () => {
             <div className='container-fluid'>
                 <div className='row'>
                     <div className='navbar-bg d-flex flex-row justify-content-around align-items-center'>
-                        <a href="/" className='d-none d-md-inline'><img src="https://assets.ccbp.in/frontend/react-js/logo-img.png" className='navbar-logo-section' alt="navbar-logo" /></a>
+                        <Link to="/" className='d-none d-md-inline'><img src="https://assets.ccbp.in/frontend/react-js/logo-img.png" className='navbar-logo-section' alt="navbar-logo" /></Link>
                         <ul className='d-flex flex-row col-3'>
                             <Link to="/" className='navbar-links'><li>Home</li></Link>
                             <Link to="/jobs" className='navbar-links'><li>Jobs</li></Link>
